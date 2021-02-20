3ce19bf89a74195106d024ca6b009a19ede20f06
christianh17 <github@familie-herrmann.de>
2021-02-20 18:44:28 +0100

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+    "bydhvs adapter settings": {
+        "en": "Adapter settings for bydhvs",
+        "de": "Adaptereinstellungen für bydhvs",
+        "ru": "Настройки адаптера для bydhvs",
+        "pt": "Configurações do adaptador para bydhvs",
+        "nl": "Adapterinstellingen voor bydhvs",
+        "fr": "Paramètres d'adaptateur pour bydhvs",
+        "it": "Impostazioni dell'adattatore per bydhvs",
+        "es": "Ajustes del adaptador para bydhvs",
+        "pl": "Ustawienia adaptera dla bydhvs",
+        "zh-cn": "bydhvs的适配器设置"
+    },
+    "option1": {
+        "en": "option1",
+        "de": "option1",
+        "ru": "option1",
+        "pt": "option1",
+        "nl": "option1",
+        "fr": "option1",
+        "it": "option1",
+        "es": "option1",
+        "pl": "option1",
+        "zh-cn": "option1"
+    },
+    "option2": {
+        "en": "option2",
+        "de": "option2",
+        "ru": "option2",
+        "pt": "option2",
+        "nl": "option2",
+        "fr": "option2",
+        "it": "option2",
+        "es": "option2",
+        "pl": "option2",
+        "zh-cn": "option2"
+    }
+};
