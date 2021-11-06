5774f4c8fc875c12e70c6efefec851275b97c907
christianh17 <github@familie-herrmann.de>
2021-11-06 12:11:49 +0100

    changed test script

--- test/integration.js
+++ test/integration.js
@@ -2,4 +2,9 @@ const path = require("path");
 const { tests } = require("@iobroker/testing");
 
 // Run integration tests - See https://github.com/ioBroker/testing for a detailed explanation and further options
-tests.integration(path.join(__dirname, ".."));
+//tests.integration(path.join(__dirname, ".."));
+
+// Run tests
+tests.packageFiles(path.join(__dirname, ".."));
+//                 ~~~~~~~~~~~~~~~~~~~~~~~~~
+// This should be the adapter's root directory
