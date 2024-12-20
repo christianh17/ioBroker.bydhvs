72aa9b3a29e782e4441a6008ed42d6739c8e6aa8
ticaki <ticaki@renopoint.de>
2024-12-20 19:09:24 +0100

    update eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,32 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-
+// ioBroker eslint template configuration file for js and ts files
+// Please note that esm or react based modules need additional modules loaded.
+import config from '@iobroker/eslint-config';
 
 export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
+    ...config,
+
+    {
+        // specify files to exclude from linting here
+        ignores: [
+            '.dev-server/',
+            '.vscode/',
+            '*.test.js', 
+            'test/**/*.js', 
+            '*.config.mjs', 
+            'build', 
+            'admin/build', 
+            'admin/words.js',
+            'admin/admin.d.ts',
+            '**/adapter-config.d.ts'     
+        ] 
+    },
+
+    {
+        // you may disable some 'jsdoc' warnings - but using jsdoc is highly recommended
+        // as this improves maintainability. jsdoc warnings will not block buiuld process.
+        rules: {
+            // 'jsdoc/require-jsdoc': 'off',
+        },
+    },
+    
 ];
