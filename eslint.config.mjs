ec41751a8560ca829b4f367b3bea93c79f936273
arteck <6681528+arteck@users.noreply.github.com>
2026-04-14 09:02:12 +0200

    big update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,30 +4,36 @@ import config from '@iobroker/eslint-config';
 
 export default [
     ...config,
-
+   
     {
         // specify files to exclude from linting here
         ignores: [
             '.dev-server/',
             '.vscode/',
-            '*.test.js', 
-            'test/**/*.js', 
-            '*.config.mjs', 
-            'build', 
+            '*.test.js',
+            'test/**/*.js',
+            '*.config.mjs',
+            'build',
+            'dist',
             'admin/build', 
             'admin/words.js',
             'admin/admin.d.ts',
-            '**/adapter-config.d.ts'     
-        ] 
+            'admin/blockly.js',
+            '**/adapter-config.d.ts',
+        ],
     },
-
     {
         // you may disable some 'jsdoc' warnings - but using jsdoc is highly recommended
         // as this improves maintainability. jsdoc warnings will not block buiuld process.
         rules: {
-            // 'jsdoc/require-jsdoc': 'off',
-            
+             'prettier/prettier': ['off', { endOfLine: 'auto' }],
+             'jsdoc/no-blank-blocks': 'off',
+             'no-prototype-builtins': 'off',
+             '@typescript-eslint/no-unused-vars': 'warn',
+             'no-useless-escape': 'warn',
+            // 'jsdoc/require-returns-description': 'off',
+            // 'jsdoc/require-returns-check': 'off',
+                      
         },
     },
-    
 ];
